4961292e67c612e8d74d6a5d703c4d4c0f117566
Xie Yanbo <xieyanbo@gmail.com>
2017-01-05 14:03:53 +0800

    autopep8

--- scripts/mistakes/mistakes.py
+++ scripts/mistakes/mistakes.py
@@ -1,18 +1,19 @@
-#! /usr/local/bin/python
-#coding:utf-8
+#!/usr/bin/env python
+# coding:utf-8
 
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+
 def readFile(filename):
-  return file(filename, 'r').readlines()
+    return file(filename, 'r').readlines()
 
 if len(sys.argv) != 2:
-  print 'Usage: mistakes [today/yesterday/(number)/top]'
-  exit()
+    print 'Usage: mistakes [today/yesterday/(number)/top]'
+    exit()
 
 cmd = sys.argv[1]
 lines = readFile('~/.learnenglishthehardway/.mistakes')
 for line in lines:
-  print line
+    print line
